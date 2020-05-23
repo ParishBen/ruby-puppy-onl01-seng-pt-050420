15f033a52f545284690531566a0a962323ad5b13
Ben Parish <parishben13@gmail.com>
2020-05-23 23:59:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
   attr_accessor :name
   @@all= []
   
- def initialize(name)
+  def initialize(name)
    @name=name
    save
   end
@@ -13,23 +13,12 @@ class Dog
   def self.clear_all
     self.all.clear
   end
-  def self.print_all
+   def self.print_all
   @@all.collect{|dog| puts dog.name}
- end
-
-  def self.all
-    @@all
-  end
-
+   end
   def save
-    self.class.all << self
+    self.all << self
   end
-
-  #def self.create
-   # song = Song.new
-    #song.save
-    #song
-  #end
 end
  
    
